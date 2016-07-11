f8a2344a9ecf87e1a3f595861de38a726861d5d6
PokemonLabz <PokemonLabz@gmail.com>
2016-07-10 20:58:17 -0400

    use style template 3

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -93,7 +93,7 @@
                 where: whereClause
             },
             styleId: 2,
-            templateId: 2
+            templateId: 3
         });
         self.fusionTable = self.searchrecords;
         self.searchrecords.setMap(map);
